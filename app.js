199e0317043c3e590af2d119364c42cd7e03602c
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2022-05-02 22:38:44 -0300

    commit 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 Vue.component('mi-primer-component', {
-    template: 
+    template: //html
     `    
         <h1 class="text-3xl">{{ title }}</h1>
     
@@ -10,14 +10,24 @@ Vue.component('mi-primer-component', {
 });
 
 Vue.component('mi-segundo-component', {
-    template: 
+    template: //html
     `    
+        
         <h1 class="text-3xl">{{ head }}</h1>
+        
+        <div v-for="(item, key) of productos" :key="key">
+            <img src="{{ item.url }}">
+            <p>{{ item.descripcion }}</p>
+            <p>{{ item.precio }}</p>
+        </div>
     
     `,
     props: {
         head: {
             type: String
+        },
+        productos: {
+            type: Object
         }
     }
 });
@@ -30,8 +40,26 @@ const app = new Vue ({
     data() {
         return {
             title: 'Mi primer componente con VueJs CDN',
-            head: 'Mi segundo componente con VueJs CDN'
+            head: 'Mi segundo componente con VueJs CDN',
+            productos: 
+            [
+                {
+                    url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
+                    descripcion: 'Hamburguesa doble-1',
+                    precio: 100
+                },
+                {
+                    url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
+                    descripcion: 'Hamburguesa doble-2',
+                    precio: 200
+                },
+                {
+                    url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
+                    descripcion: 'Hamburguesa doble-3',
+                    precio: 300
+                }
+            ]
         }
-    },
+    }
     
 });

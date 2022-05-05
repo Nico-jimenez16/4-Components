8c0b3d434d6f28a3315bc1db7be6aa9b5339915b
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2022-05-05 14:16:59 -0300

    arreglo de pops y add header

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 Vue.component('mi-primer-component', {
-    template: //html
-    `    
-        <h1 class="flex justify-center items-center h-full text-3xl">{{ title }}</h1>
+    template:
+    `
+        <div>
+            <div class="flex w-full max-h-32 text-white border-b p-4">
+                <div class="flex flex-col w-full hidden md:flex items-end justify-center pr-4">
+                    <h2 class="text-2xl">{{ titulo }}</h2>
+                    <a class="text-xl"><strong></strong>{{ entrega }}</a>
+                </div>
+            </div>
+        </div> 
     
     `,
     // props: [
@@ -9,17 +16,20 @@ Vue.component('mi-primer-component', {
     // ]
 
     props: {
-        title: {
-            title: String
+        titulo: {
+            type: String
+        },
+        entrega: {
+            type: String
         }
     }
 });
 
 Vue.component('mi-segundo-component', {
-    template: //html
+    template:
     `    
         <div>
-            <h1 class="flex justify-center text-4xl mb-4">{{ head }}</h1>
+            <h1 class="flex justify-center text-4xl mb-4">{{ hprod }}</h1>
             <div class="flex flex-wrap w-full justify-center bg-amber-50 items-center">
                 <div v-for="(item, i) of productos" :key="i" class="flex-col m-8 border-[#fff]">
                     <img :src="item.url">
@@ -30,10 +40,9 @@ Vue.component('mi-segundo-component', {
                 </div>
             </div>
         </div>
-    
     `,
     props: {
-        head: {
+        hprod: {
             type: String
         },
         productos: {
@@ -47,8 +56,9 @@ const app = new Vue ({
     el: '#app',
     data() {
         return {
-            title: 'Mi primer componente con VueJs CDN',
-            head: 'Componente de Productos',
+            titulo: 'Hola , soy Nicolas Jimenez',
+            entrega: 'Esta es la Segunda entrega - Componentes con VueJs',
+            hprod: 'Componente de Hamburguesas',
             productos: 
             [
                 {
@@ -60,31 +70,6 @@ const app = new Vue ({
                     url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
                     descripcion: 'Hamburguesa doble',
                     precio: 200
-                },
-                {
-                    url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
-                    descripcion: 'Hamburguesa doble',
-                    precio: 300
-                },
-                {
-                    url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
-                    descripcion: 'Hamburguesa doble',
-                    precio: 100
-                },
-                {
-                    url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
-                    descripcion: 'Hamburguesa doble',
-                    precio: 200
-                },
-                {
-                    url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
-                    descripcion: 'Hamburguesa doble',
-                    precio: 300
-                },
-                {
-                    url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
-                    descripcion: 'Hamburguesa doble',
-                    precio: 300
                 },
                 {
                     url: 'https://www.clarin.com/img/2021/05/20/la-hamburguesa-triple-cheese-bacon___Lj4vcOHr__340x340__1.jpg',
